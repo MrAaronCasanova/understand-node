f0ed7d50e34e5248434412d61b0f051ac347a3b9
MrAaronCasanova <1boris231@gmail.com>
2018-03-17 20:54:21 -0700

    more on buffer and stream

--- 5-buffer-stream/app.js
+++ 5-buffer-stream/app.js
@@ -2,7 +2,7 @@ console.log('-------------------------');
 
 // Buffer Basics
 
-var buf = new Buffer('Casanova', 'utf8');
+var buf = new Buffer('Casanova', 'utf8'); // 8bit encoding (states each character should be encoded in bytes)
 
 console.log(buf); // <Buffer 43 61 73 61 6e 6f 76 61>
 // outputs binary data (in hexadecimal notation)
@@ -25,7 +25,7 @@ buf.write('Waz');
 
 console.log(buf.toString()); // Wazanova
 
-// ---------------------------------------------------------------
+// --------------------------------------
 
 console.log('-------------------------');
 
@@ -44,3 +44,42 @@ view[0] = 9; // adds 32bit encoding of 9 to buffer
 view[1] = 23; // adds 32bit encoding of 23 to buffer
 view[2] = 923; // nothing - no room left in buffer (64bit limit)
 console.log(view); // Int32Array [9, 23]
+
+// --------------------------------------
+
+// Streams in Node
+
+console.log('-------------------------');
+console.log('-------------------------');
+
+// core node file system module
+var fs = require('fs');
+
+// createReadStream returns on object
+// reads the specified file and fills a buffer
+var readable = fs.createReadStream(__dirname + '/greet.txt');
+// if file is bigger than buffer the data will be split into chunks - default buffer is 64KB
+
+// event emitted by the read stream is 'data'
+readable.on('data', function(chunk) {
+  // .on methods is available to createReadStream as it inherits from the EventEmitter
+  console.log(chunk);
+  console.log('-------------------------');
+});
+
+// specifying option to get strings instead of buffers
+var readableString = fs.createReadStream(__dirname + '/greet.txt', {
+  // encodes binary data in the stream
+  encoding: 'utf8',
+  // specifies the number of bytes we want our buffer size to be (chunk size)
+  highWaterMark: 1024 // 1KB
+});
+
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+module.exports = readableString.on('data', function(chunk) {
+  console.log(chunk); // chunk of utf8 encoded string
+  console.log(chunk.length + 'based on highWaterMark');
+  console.log('****************************');
+  writable.write(chunk);
+});
