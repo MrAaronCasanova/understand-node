99c63bbf688c5fc3f292f411385c837161bb79fd
MrAaronCasanova <1boris231@gmail.com>
2018-03-19 10:11:54 -0700

    basics of sending templated html

--- 6-http-web-server/6a-server-basics/app.js
+++ 6-http-web-server/6a-server-basics/app.js
@@ -1,9 +1,9 @@
 // grab core http module
 var http = require('http');
 
-// .createServer is an http method to create a new server object
+// .createServer is an http method to create a new Server object
 // method takes a callback which is actually an EventListener
-// when server object emits a certain event, the callback runs
+// when Server object emits a certain event, the callback runs (requestListener)
 http
   .createServer(function(req, res) {
     // building the head of the http response
@@ -13,11 +13,9 @@ http
       'Content-Type': 'text-plain'
     });
     // there is a .send method, but .end indicates it the last thing we are sending
-    res.end('Hello World\n');
+    res.end('Hello World\n'); // \n stand for carriage return (good practice for ending data)
   })
   .listen(1337, '127.0.0.1');
-// node maps the port via .listen to the server object return by .createServer
+// node maps the port via .listen to the Server object returned by .createServer
 // 127... standard internal IP address for local system/localhost
 // when browser makes request to 127.0.0.1:1337 node will make sure the process is routed properly to the above code
-
-// Left off - Folder: 7 _ Video: 60
---
+++ 6-http-web-server/6b-server-html/app.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+http
+  .createServer(function(req, res) {
+    // reads/stores index.htm off file system - returns buffer by default
+    // specify utf8 to decode binary stream to a string of HTML instead of a buffer
+    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    var message = 'Hello World!!'; // example template content
+    html = html.replace('{Message}', message); // updated string
+    // .replace method was available because html was converted to a string
+
+    // build/sends the http header
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    // builds/sends the string of html
+    res.end(html);
+  })
+  .listen(1337, '127.0.0.1');
