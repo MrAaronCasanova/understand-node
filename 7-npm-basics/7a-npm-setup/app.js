a70db32745e9ecf12678750276193e8ffe887155
MrAaronCasanova <1boris231@gmail.com>
2018-03-20 13:55:27 -0700

    npm basics and setup

---
+++ 7-npm-basics/7a-npm-setup/app.js
@@ -0,0 +1,6 @@
+// require first looks in nodes core modules
+// but also looks for a node_modules folder in your app (...and more locations TBD)
+var moment = require('moment');
+console.log(moment().format('ddd, h:mA'));
+
+// vid 73!!!
