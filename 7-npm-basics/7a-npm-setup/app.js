5e2e3598378517abdc903337cb3e0f3eaa0d8c40
MrAaronCasanova <1boris231@gmail.com>
2018-03-20 14:50:26 -0700

    understanding express and middleware

--- 7-npm-basics/7a-npm-setup/app.js
+++ 7-npm-basics/7a-npm-setup/app.js
@@ -2,5 +2,3 @@
 // but also looks for a node_modules folder in your app (...and more locations TBD)
 var moment = require('moment');
 console.log(moment().format('ddd, h:mA'));
-
-// vid 73!!!
---
+++ 8-express/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+// middleware examples
+// the function will run inbetween the request and response of the specified route
+// vvv custom middleware
+app.use('/api', function(req, res, next) {
+  console.log('I am the /api route middleware!!');
+  next();
+});
+// if no route is specified the function will be called for every request
+// vvv (standard) for serving you public/static files
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render(index.html);
+});
+
+// utilizing route params/variable (:arbitrary-string)
+app.get('/person/:name', function(req, res) {
+  res.send('Hello ' + req.params.name); // route params can be accessed through req.params
+});
+
+app.get('/api', function(req, res) {
+  // automatically converts object to json
+  res.json({
+    firstname: 'Aaron',
+    lastname: 'Casanova'
+  });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port: ' + port);
+});
